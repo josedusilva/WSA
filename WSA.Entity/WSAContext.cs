31a5494606068118ecacdba8e038712b76885cdb
jose.silva <jose.silva@opentechgr.com.br>
2018-06-18 17:40:17 -0300

    novo fim

---
+++ WSA.Entity/WSAContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WSA.Model;
+
+namespace WSA.Entity
+{
+    public class WSAContext : DbContext
+    {
+        public DbSet<Associado> Associados { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<Associado>()
+                .Property(a => a.Id)
+                .HasColumnName("cdAssociado");
+
+            modelBuilder
+                .Entity<Associado>()
+                .Property(a => a.Nome)
+                .HasColumnName("dsNome")
+                .HasColumnType("varchar(45)")
+                .IsRequired();
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ WSA.Model/Associado.cs
@@ -0,0 +1,8 @@
+﻿namespace WSA.Model
+{
+    public class Associado
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+    }
+}
